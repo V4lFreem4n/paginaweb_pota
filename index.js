f30a45a09cd5e13d851df3e3e58ce83e8c73666b
Victor Purizaca <golgisaparato74@gmail.com>
2024-11-03 01:40:30 -0500

    mi quinto commit

--- index.js
+++ index.js
@@ -40,5 +40,50 @@ function animacion() {
   }
 
   window.addEventListener("load", animacion);
+  let estado = false;
+  function cambiarMisionVision(){
+    console.log("Se llegó acá")
+    console.log(estado)
+    
+    let divDescription1 = document.getElementById('square-description-1');
+    let divDescription2 = document.getElementById('square-description-2');
+     
+    let divImage1 = document.getElementById('square-image-1');
+    let divImage2 = document.getElementById('square-image-2');
 
- 
+    if (estado) {
+          divDescription1.classList.remove('mover');
+          divDescription2.classList.remove('volver');
+
+          setTimeout(function() {
+            divImage1.classList.remove('mover');
+            divImage2.classList.remove('volver'); 
+          }, 500); 
+
+          
+            estado = false;
+        } else {
+          divDescription1.classList.add('mover');
+          divDescription2.classList.add('volver');
+
+          setTimeout(function() {
+            divImage1.classList.add('mover');
+            divImage2.classList.add('volver'); 
+        }, 500); 
+
+          
+            estado = true;
+        }
+    };
+  
+
+  
+
+
+
+
+
+
+
+
+
